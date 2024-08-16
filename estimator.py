b94de5de714cb0152d5e55af979d2391a45c0437
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-08-17 00:09:46 +0100

    add abstract fit predict and data prep methods in estimator.py

--- estimator.py
+++ estimator.py
@@ -6,8 +6,11 @@ from typing import Dict, Any
 import pandas as pd
 import optuna
 import pytz
+from abc import ABC, abstractmethod
+from evaluation_utils import calculate_all_metrics
 
-class Estimator:
+
+class Estimator(ABC):
     def __init__(self, name: str, base_dir: str):
         self.name = name
         self.base_dir = base_dir
@@ -27,14 +30,68 @@ class Estimator:
             optimization = False
             n_trials = 50
 
+    @abstractmethod
+    def fit(self, train_data: Dict[str, pd.DataFrame]):
+        """
+        Fit the model on the training data.
+
+        :param train_data: Dictionary containing training data
+        """
+        pass
+
+    @abstractmethod
+    def predict(self, test_data: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+        """
+        Make predictions using the fitted model.
+
+        :param test_data: Dictionary containing test data
+        :return: DataFrame with predictions
+        """
+        pass
+
+    @abstractmethod
+    def prepare_data(self, data: Dict[str, pd.DataFrame]) -> Dict[str, Any]:
+        """
+        Prepare the data for fitting or prediction.
+
+        :param data: Dictionary containing raw data
+        :return: Dictionary containing prepared data
+        """
+        pass
+
+    @abstractmethod
+    def define_hyperparameter_space(self, trial: optuna.Trial) -> Dict[str, Any]:
+        """
+        Define the hyperparameter space for optimization.
+
+        :param trial: Optuna trial object
+        :return: Dictionary of hyperparameters
+        """
+        pass
+
+    @abstractmethod
+    def calculate_all_performance_metrics(self, predictions: pd.DataFrame, actuals: pd.DataFrame,
+                                          naive_forecast: pd.DataFrame) -> Dict[str, float]:
+        """
+        Calculate all performance metrics.
+
+        :param predictions: DataFrame of predictions
+        :param actuals: DataFrame of actual values
+        :param naive_forecast: DataFrame of naive forecast
+        :return: Dictionary of metric names and values
+        """
+        return calculate_all_metrics(predictions, actuals, naive_forecast)
+
     def optimize(self, train_data: Dict[str, pd.DataFrame], valid_data: Dict[str, pd.DataFrame], n_trials: int = 50):
         @self.ex.main
         def optimization_run(_run, n_trials):
             def objective(trial):
                 params = self.define_hyperparameter_space(trial)
                 self.hyperparameters = params
-                self.fit(train_data)
-                predictions = self.predict(valid_data)
+                prepared_train_data = self.prepare_data(train_data)
+                self.fit(prepared_train_data)
+                prepared_valid_data = self.prepare_data(valid_data)
+                predictions = self.predict(prepared_valid_data)
                 metrics = self.calculate_all_performance_metrics(predictions, valid_data['day_ahead_prices'],
                                                                  valid_data['naive_forecast'])
                 _run.log_scalar("trial", trial.number)
