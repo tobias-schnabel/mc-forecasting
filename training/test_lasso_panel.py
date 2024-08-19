31ac46ed8c2ec57f8bc701ab242d382b776c31d7
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-08-19 23:23:25 +0100

    change estimator name for lasso in test_lasso_panel.py

--- training/test_lasso_panel.py
+++ training/test_lasso_panel.py
@@ -6,7 +6,7 @@ setup_paths, DataLoader, ForecastEngine = setup_environment()
 from models.lasso_panel import LEAREstimator
 data_dir, base_dir, tuning_dir, results_dir = setup_paths('scratch')
 data_loader = DataLoader(data_dir)
-estimator = LEAREstimator("LEAR_MV", results_dir, use_db=True)
+estimator = LEAREstimator("Lasso_panel", results_dir, use_db=True)
 engine = ForecastEngine(data_loader, [estimator])
 engine.min_train_window = 8
 start_date = pd.Timestamp("2019-01-01", tz='UTC')
