0f20efcb9191226aecb53e31d9cc355c6d9c5bc4
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-09-02 23:44:22 +0100

    Implement MC-NNM including optimization

--- training/test_mcnnm.py
+++ training/test_mcnnm.py
@@ -9,7 +9,7 @@ data_dir, base_dir, tuning_dir, results_dir = setup_paths('scratch')
 data_loader = DataLoader(data_dir)
 estimator = MCNNMEstimator("mcnnm", results_dir, use_db=True)
 engine = ForecastEngine(data_loader, [estimator])
-# engine.min_train_window =7
+
 estimator.X = engine.data_loader.installed_capacity
 start_date = pd.Timestamp("2019-01-01", tz='UTC')
 end_date = pd.Timestamp("2019-01-31", tz='UTC')
