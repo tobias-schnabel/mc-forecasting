81d1206fc9f87058d91918eae578bcc945815f2c
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-09-05 18:08:18 +0100

    split up training scripts

--- training/train_elasticnet.py
+++ training/train_elasticnet.py
@@ -2,19 +2,15 @@ import pandas as pd
 
 from data_utils import setup_environment
 from models.elastic_net_regression import ElasticNetEstimator
-from models.lasso_panel import LASSOEstimator
-from models.lear_panel import LEAREstimator
 
 setup_paths, DataLoader, ForecastEngine = setup_environment()
 
 data_dir, base_dir, tuning_dir, results_dir = setup_paths()
 data_loader = DataLoader(data_dir)
 
-lasso = LASSOEstimator("LASSO_panel", results_dir, use_db=True)
-lear = LEAREstimator("LEAR_MV", results_dir, use_db=True)
-en = ElasticNetEstimator("ElasticNet", results_dir, use_db=True)
+en = ElasticNetEstimator("ElasticNet", results_dir, use_db=False)
 
-engine = ForecastEngine(data_loader, [en, lasso, lear])
+engine = ForecastEngine(data_loader, [en])
 
 start_date = pd.Timestamp("2019-01-01", tz='UTC')
 end_date = pd.Timestamp("2024-06-30", tz='UTC')
