34b4a30cc52dec4d0a63c00b2dff9a0583214c22
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-09-07 19:02:29 +0200

    set up training scripts to train all linear models with 56, 84, 112 days sliding windows (unlimited expanding window takes too long)

---
+++ training/train_elasticnet_84.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from data_utils import setup_environment
+from models.elastic_net_regression import ElasticNetEstimator
+
+setup_paths, DataLoader, ForecastEngine = setup_environment()
+
+data_dir, base_dir, tuning_dir, results_dir = setup_paths()
+data_loader = DataLoader(data_dir)
+
+en = ElasticNetEstimator("ElasticNet-84", results_dir, use_db=False)
+
+engine = ForecastEngine(data_loader, [en])
+engine.max_train_window = 84
+
+start_date = pd.Timestamp("2019-01-01", tz='UTC')
+end_date = pd.Timestamp("2024-06-30", tz='UTC')
+
+try:
+    engine.run_forecast(start_date, end_date)
+except KeyboardInterrupt:
+    print("Training interrupted. Saving final results...")
+    engine._save_final_results()
+    engine._save_final_hyperparameters()
---
+++ training/train_lasso_112.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from data_utils import setup_environment
+from models.lasso_panel import LASSOEstimator
+
+setup_paths, DataLoader, ForecastEngine = setup_environment()
+
+data_dir, base_dir, tuning_dir, results_dir = setup_paths()
+data_loader = DataLoader(data_dir)
+
+lasso = LASSOEstimator("LASSO-112", results_dir, use_db=False)
+
+engine = ForecastEngine(data_loader, [lasso])
+engine.max_train_window = 112
+
+start_date = pd.Timestamp("2019-01-01", tz='UTC')
+end_date = pd.Timestamp("2024-06-30", tz='UTC')
+
+try:
+    engine.run_forecast(start_date, end_date)
+except KeyboardInterrupt:
+    print("Training interrupted. Saving final results...")
+    engine._save_final_results()
+    engine._save_final_hyperparameters()
