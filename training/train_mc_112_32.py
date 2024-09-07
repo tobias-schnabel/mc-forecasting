e5d1dffc03a6197decdc04449a8c985dd0a698ed
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-09-07 18:45:17 +0200

    rename training scripts and existing results

--- training/train_mc_112_32.py
+++ training/train_mc_112_32.py
@@ -4,17 +4,17 @@ import pandas as pd
 jax.config.update("jax_enable_x64", False)  # set 32-bit precision for all floats
 
 from data_utils import setup_environment
-from models.mcnnm_32 import MCNNMEstimator
+from models.mcnnm_32_bit import MCNNMEstimator
 
 setup_paths, DataLoader, ForecastEngine = setup_environment()
 
 data_dir, base_dir, tuning_dir, results_dir = setup_paths()
 data_loader = DataLoader(data_dir)
 
-mc = MCNNMEstimator("MCNNM-32-84", results_dir, use_db=False)
+mc = MCNNMEstimator("MCNNM-112_32", results_dir, use_db=False)
 
 engine = ForecastEngine(data_loader, [mc])
-engine.max_train_window = 84
+engine.max_train_window = 112
 
 start_date = pd.Timestamp("2019-01-01", tz='UTC')
 end_date = pd.Timestamp("2024-06-30", tz='UTC')
